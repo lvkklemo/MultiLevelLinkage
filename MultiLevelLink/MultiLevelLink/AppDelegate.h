76feed006e7b2750dd08df5832d8d71f5d5e0be1
lkk2016 <631813042@qq.com>
2016-12-09 15:51:58 +0800

    千万

---
+++ MultiLevelLink/MultiLevelLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiLevelLink
+//
+//  Created by 宇航 on 16/12/9.
+//  Copyright © 2016年 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
